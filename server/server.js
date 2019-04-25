ece92d5dde62a88db3081fe19faa882e3b22b66c
Chanon <5910110071@psu.ac.th>
2019-04-26 02:56:33 +0700

    PUT

--- server/server.js
+++ server/server.js
@@ -6,24 +6,32 @@ var bodyParser = require('body-parser');
 var bears = [];
 
 router.route('/bears')
-    .post(function(req,res){
+    .post(function (req, res) {
         var bear = {};
         bear.name = req.body.name;
         bears.push(bear);
-        res.json({ message : 'Bear crated!' });
+        res.json({ message: 'Bear crated!' });
 
     });
 
 router.route('/bears')
-    .get(function(req,res){
+    .get(function (req, res) {
         res.send(bears)
     });
 
 router.route('/bears/:bear_id')
-    .get(function (req,res) {
+    .get(function (req, res) {
         var id = req.params.bear_id;
         res.send(bears[id])
     });
 
-    app.use('/api',bodyParser.json(),router);
-    app.listen(8000);
+router.route('/bears/:bear_id')
+    .put(function (req, res) {
+        var id = req.params.bear_id;
+        bears[id].name = req.body.name;
+        bears[id].id = req.body.id;
+        res.send(bears[id]);
+    });
+
+app.use('/api', bodyParser.json(), router);
+app.listen(8000);
