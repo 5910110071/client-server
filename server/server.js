f1d1c21d824e4fe1830b2978a934daa51e27a80b
Chanon <5910110071@psu.ac.th>
2019-04-26 04:24:12 +0700

    post get + firebase

--- server/server.js
+++ server/server.js
@@ -2,22 +2,41 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
+var firebase = require('firebase');
+
+var config = {
+    apiKey: "AIzaSyC6zT9cYycz-EU-DNAQcaoO05DlTxmgfZg",
+    authDomain: "client-server-e0e66.firebaseapp.com",
+    databaseURL: "https://client-server-e0e66.firebaseio.com",
+    projectId: "client-server-e0e66",
+    storageBucket: "client-server-e0e66.appspot.com",
+    messagingSenderId: "800996448549"
+  };
+  firebase.initializeApp(config);
 
 var bears = [];
 
 router.route('/bears')
     .post(function (req, res) {
-        var bear = {};
-        bear.name = req.body.name;
-        bear.id = req.body.id;
-        bears.push(bear);
+        // var bear = {};
+        bear_name = req.body.name;
+        bear_id = req.body.id;
+        //bears.push(bear);
+        firebase.database().ref('bears/' + bear_id ).set({
+            name : bear_name,
+            id : bear_id
+        });
         res.json({ message: 'Bear crated!' });
 
     });
 
 router.route('/bears')
     .get(function (req, res) {
-        res.send(bears)
+        //res.send(bears)
+        var bear_path = firebase.database().ref('bears/');
+        bear_path.on('value',function(snapshot){
+            res.send(snapshot.val())
+        });
     });
 
 router.route('/bears/:bear_id')
